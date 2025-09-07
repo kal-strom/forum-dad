ac1c8daa5ff24d6312a4f196a602249ad7dfd674
KalKal <kalkalkalz@proton.me>
2025-09-07 16:23:15 -0700

    Added a views folder, moved index.html into it.

---
+++ server.js
@@ -0,0 +1,14 @@
+
+// loads the express library into our project
+const express = require('express');
+
+// creating an instance of express named app that represents our server
+const app = express();
+
+// setting the port number the server "listens on"
+// visiting http://localhost:3000 will connect to it 
+const port = 3000;
+
+
+
+
