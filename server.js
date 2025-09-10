e72cf252e5e269c9aa4a211949f3a1f34273a68c
KalKal <kalkalkalz@proton.me>
2025-09-09 18:02:06 -0700

    installed express-session module, implemented it and bcrypt into server.js

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 // loads the express library into our project
 const express = require('express');
 
+// importing express-session module
+// need to configure 
+const session = requrie('express-session');
+
 // creating an instance of express named app that represents our server
 const app = express();
 
 // setting the port number the server "listens on"
 // visiting http://localhost:3000 will connect to it 
 const PORT = 3000;
+
+// importing bcrypt module
+// can now use bcrypt.hash & bcrypt.compare
+// to hash and compare passwords for storage
+const bcrypt = require('bcrypt');
+
 
 // telling our express app to use middleware
 // if the request body contains JSON data,
@@ -25,4 +35,3 @@ app.listen(PORT, () =>
 app.get('/', (req,res) => {
     res.send('Hello World')
 });
-
